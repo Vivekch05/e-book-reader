b725cf0ef7428cee177056bf0d261a0c09e3916d
Vivek Kumar Chaurasia <vivek.chaurasia@synapse.com>
2020-07-07 10:17:15 +0530

    Updates

---
+++ precache-manifest.c2bbf3c9bba72371ae47e45771b1874a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e852152564ce8af8851ed8859dfbb3fc",
+    "url": "/e-book-reader/index.html"
+  },
+  {
+    "revision": "65c26f6a316f36722cd9",
+    "url": "/e-book-reader/static/css/2.4e1b8863.chunk.css"
+  },
+  {
+    "revision": "05bb97556df826bf35ae",
+    "url": "/e-book-reader/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "65c26f6a316f36722cd9",
+    "url": "/e-book-reader/static/js/2.883a48a5.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/e-book-reader/static/js/2.883a48a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05bb97556df826bf35ae",
+    "url": "/e-book-reader/static/js/main.62e83229.chunk.js"
+  },
+  {
+    "revision": "64be4ffc744942535fca",
+    "url": "/e-book-reader/static/js/runtime-main.c9231846.js"
+  },
+  {
+    "revision": "5dd58a6e369e9592e7ad4fb839942789",
+    "url": "/e-book-reader/static/media/book20.5dd58a6e.jpg"
+  },
+  {
+    "revision": "c22b31eba715c8bf0c229ac4627c4d38",
+    "url": "/e-book-reader/static/media/book25.c22b31eb.jpg"
+  },
+  {
+    "revision": "d3eaf4838f2b5336d19d0a2c2cd52e8f",
+    "url": "/e-book-reader/static/media/book26.d3eaf483.jpg"
+  },
+  {
+    "revision": "32205ff7d534add015babf45efa3ac18",
+    "url": "/e-book-reader/static/media/book31.32205ff7.jpg"
+  },
+  {
+    "revision": "54df4bfce612b29d64a55a9b1f28e59b",
+    "url": "/e-book-reader/static/media/logo1.54df4bfc.jpg"
+  }
+]);
